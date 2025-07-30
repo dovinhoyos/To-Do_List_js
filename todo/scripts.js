ca41ca4654e3440baa8149a56f603caecd13a17f
dovinhoyos <dovricarhoy@gmail.com>
2025-07-30 17:46:43 -0500

    cambios!

--- todo/scripts.js
+++ todo/scripts.js
@@ -3,9 +3,16 @@ const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 const filterSelect = document.getElementById("filterSelect");
 
+const getTasksFromStorage = () => {
+  return JSON.parse(localStorage.getItem("tasks")) || []
+}
+
+const saveTasksToStorage = (tasks) => {
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
 const createTaskElement = (task) => {
   const li = document.createElement("li");
-  console.log(li)
   li.classList.add("task");
   li.dataset.id = task.id;
 
@@ -22,7 +29,9 @@ const createTaskElement = (task) => {
   deleteBtn.textContent = "🗑";
   deleteBtn.classList.add("delete-btn");
 
-  if (task.completed) li.classList.add("completed");
+  if (task.completed){
+    li.classList.add("completed")
+  };
 
   li.appendChild(checkbox);
   li.appendChild(span);
@@ -47,9 +56,9 @@ const handleAddTask = () => {
   const taskItem = createTaskElement(newTask);
   taskList.appendChild(taskItem);
 
-  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  const tasks = getTasksFromStorage();
   tasks.push(newTask);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  saveTasksToStorage(tasks);
 
   taskInput.value = "";
   taskInput.focus();
@@ -92,36 +101,75 @@ taskList.addEventListener("click", (e) => {
 
   if (target.classList.contains("task-checkbox")) {
     const taskItem = target.closest(".task");
+    console.log(taskItem)
     taskItem.classList.toggle("completed");
 
     const id = taskItem.dataset.id;
-    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const tasks = getTasksFromStorage();
 
     const task = tasks.find((t) => t.id === id);
     if (task) {
       task.completed = target.checked;
-      localStorage.setItem("tasks", JSON.stringify(tasks));
+      saveTasksToStorage(tasks);
     }
   }
 
   if (target.classList.contains("delete-btn")) {
     const taskItem = target.closest(".task");
-    console.log(taskItem);
     const id = taskItem.dataset.id;
 
     taskItem.classList.add("fade-out");
     setTimeout(() => taskItem.remove(), 300);
 
-    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const tasks = getTasksFromStorage();
     const updated = tasks.filter((t) => t.id !== id);
-    localStorage.setItem("tasks", JSON.stringify(updated));
+    saveTasksToStorage(updated);
   }
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  const tasks = getTasksFromStorage();
   tasks.forEach((task) => {
     const taskItem = createTaskElement(task);
     taskList.appendChild(taskItem);
   });
 });
+
+
+// const tablabody = document.querySelector("#tablabody")
+
+// const tareas = [
+//   {nombre: "Carlos", edad: 23},
+//   {nombre: "Juan", edad: 23},
+//   {nombre: "Camilo", edad: 23},
+//   {nombre: "Pepe", edad: 23},
+//   {nombre: "Ruben", edad: 23}
+// ]
+
+// tareas.forEach((t) => {
+//   const tr = document.createElement("tr")
+//   tr.classList.add("pendiente")
+
+//   tr.innerHTML = `<td >${t.nombre}</td> <td>${t.edad}</td>`
+
+//   tablabody.appendChild(tr)
+// })
+
+// const nuevaTarea = {
+//   nombre: "Santiago",
+//   edad: "34"
+// }
+
+// const nuevasTareas = [...tareas, nuevaTarea]
+
+// console.log(nuevasTareas)
+// console.log(tareas)
+
+// const localTareas = getTasksFromStorage()
+// localTareas.push(nuevaTarea)
+// saveTasksToStorage(localTareas)
+
+// tr.classList.contains("pendiente")
+
+
+
